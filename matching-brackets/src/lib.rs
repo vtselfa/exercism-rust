c55b2af621c5311a3c42e726044149e5b6cf0efa
Vicent Selfa Oliver <vselfaoliver@apple.com>
2020-03-16 11:16:45 +0100

    Matching brackets alternative solution

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -12,17 +12,12 @@ pub fn brackets_are_balanced(string: &str) -> bool {
 
     let mut stack = Vec::<char>::new();
     for new in string.chars().filter(|x| tokens.contains(x)) {
-        match stack.last() {
-            None => stack.push(new),
-            Some(last) => {
-                // If `last` was an open token, and `new` is the corresponding close token we pop
-                // `last` from the stack
-                if open_to_close.contains_key(last) && open_to_close[last] == new {
-                    stack.pop();
-                } else {
-                    stack.push(new);
-                }
-            }
+        // If it's an open token, we push the expected close token in the stack
+        if open_to_close.contains_key(&new) {
+            stack.push(open_to_close[&new]);
+        // If it´s a close token, there must be an equal one in the top of the stack
+        } else if stack.pop() != Some(new) {
+            return false;
         }
     }
     stack.is_empty()
