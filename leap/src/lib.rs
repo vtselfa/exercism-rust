7e685b5e33babd3b48d078c94da719152e1211bb
Vicent Selfa Oliver <vselfaoliver@apple.com>
2020-03-18 10:38:16 +0100

    Leap completed

---
+++ leap/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn is_leap_year(year: u64) -> bool {
+    // on every year that is evenly divisible by 4
+    // except every year that is evenly divisible by 100
+    // unless the year is also evenly divisible by 400
+
+    if year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) {
+        true
+    } else {
+        false
+    }
+}
